1f57fbcf9e87ef5b8987288a98c3fea6798cb89c
leojiang <1075001194@qq.com>
2021-02-06 17:08:33 +0800

    enter

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import fastclick from 'fastclick'
 import 'styles/iconfont.css'
+import 'swiper/swiper-bundle.css'
+import Swiper, { Pagination, Navigation } from 'swiper'
+Swiper.use([Pagination, Navigation])
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="wrapper">
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" alt="抱歉，图片加载错误">
+    </swiper-slide>
+    <!-- 用来显示分页的内容 -->
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOptions: {
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://img1.qunarzz.com/qs/2101/87/f3fa48c8346dab02.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://img1.qunarzz.com/qs/2101/e8/3823364f76ded302.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'https://img1.qunarzz.com/qs/2101/26/bd4db8b1e8de6302.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+//    >>>无视scoped的影响，进行样式穿透
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+  .wrapper
+      overflow:hidden
+      width: 100%;
+      height: 50vw
+      .swiper-img
+          width:100%
+</style>
