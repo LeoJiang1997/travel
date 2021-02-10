bd99926f96aceb8ee351e1249c7e4d3939ba3a1e
leojiang <1075001194@qq.com>
2021-02-10 15:16:39 +0800

    city-search-logic

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,8 +1,8 @@
 <template>
    <div>
      <city-header></city-header>
-     <city-search></city-search>
-     <city-list :cities="cities" :hot="hotCities"></city-list>
+     <city-search :cities="cities"></city-search>
+     <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
      <city-alphabet :cities="cities"></city-alphabet>
    </div>
 </template>
@@ -24,7 +24,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
