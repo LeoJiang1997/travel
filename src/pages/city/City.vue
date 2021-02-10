085b917e4af47ea567ebf550818b7d6eed981c7b
leojiang <1075001194@qq.com>
2021-02-10 14:14:29 +0800

    performance

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,10 +1,16 @@
 <template>
-   <div>
-     <city-header></city-header>
-     <city-search></city-search>
-     <city-list :cities="cities" :hot="hotCities"></city-list>
-     <city-alphabet :cities="cities"></city-alphabet>
-   </div>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+    <city-list :cities="cities"
+               :hot="hotCities"
+               :letter="letter">
+    </city-list>
+    <city-alphabet
+       :cities="cities"
+       @change="handleLetterChange"
+    ></city-alphabet>
+  </div>
 </template>
 
 <script>
@@ -24,7 +30,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -39,7 +46,11 @@ export default {
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
+
   },
   mounted () {
     this.getCityInfo()
